105749ded61599b44f4d88cbf55f4438c0a3f378
Uzeyr OZCAN <muzeyr@gmail.com>
2020-01-22 16:58:55 +0300

    Updates

---
+++ precache-manifest.b25cdc3169527b0230435b48557e88a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8fcac7d227897e965f7a1dd5e4b723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05e571926c34df3a2e9b",
+    "url": "/static/css/main.d23f80e8.chunk.css"
+  },
+  {
+    "revision": "94f4e73ca54297ab2a78",
+    "url": "/static/js/2.cf5bcb4c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.cf5bcb4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05e571926c34df3a2e9b",
+    "url": "/static/js/main.3f773c14.chunk.js"
+  },
+  {
+    "revision": "c7bba16c195430e78514",
+    "url": "/static/js/runtime-main.baff40b1.js"
+  }
+]);
