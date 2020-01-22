859bd8c4f4d0d4b506e73401d743487c548c2b7e
Uzeyr OZCAN <muzeyr@gmail.com>
2020-01-22 15:25:20 +0300

    Card robobash added image

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,5 +3,6 @@ import './card.styles.css';
 
 export const Card = props => (
    <div className="card-container">
+       <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} ></img>
        <h1>{props.monster.name}</h1>
    </div>);
