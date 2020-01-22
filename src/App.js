beafbb99ce0c545a287fc76e973a561784d4d9ff
Uzeyr OZCAN <muzeyr@gmail.com>
2020-01-22 14:16:01 +0300

    Monster example

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        { name: 'Frankenstein', id: 1 },
+        { name: 'Dracula', id: 2 },
+        { name: 'Zombie', id: 3 },
+
+      ]
+    }
+
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+
+
+  }
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+
+          )
+
+          )
+
+        }
+      </div>
+    );
+  }
+
+}
+
+export default App;
